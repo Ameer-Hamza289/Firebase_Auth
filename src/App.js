6d952534f3dbfbf093de01925af88287ee47ee1a
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-05-16 22:43:09 +0500

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
-import './App.css';
+import React, { useEffect,useState } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./components/Login";
+import SignUp from "./components/Register";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Profile from "./components/Profile";
+import { auth } from "./components/Firebase";
+
 
 function App() {
+
+  const [user, setUser] = useState();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  });
+
+  console.log(user,"user");
+
   return (
+    <Router>
     <div className="App">
-     Hello from react app.
+      <div className='auth-wrapper'>
+      <div className="auth-inner">
+            <Routes>
+              <Route
+                path="/"
+                element={user ? <Navigate to="/profile" /> : <Login />}
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<SignUp />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
+            <ToastContainer />
+          </div>
+     </div>
     </div>
+    </Router>
   );
 }
 
